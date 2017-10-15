034e8a74d804e8499b5e0518d77622541f8b9f42
NikNok391 <32327321+NikNok391@users.noreply.github.com>
2017-10-16 01:39:17 +0200

    Add files via upload

---
+++ MemberRepository.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author niklaswittenbrink
+ */
+public class MemberRepository {
+    private List<Member> members = new ArrayList<Member>();
+
+    public MemberRepository() {
+    }  
+    
+    public void addMember(Member newMemb){
+       members.add(newMemb);
+    }
+    
+    public void removeMember(int id){
+        members.remove(id);
+    }
+
+    public List<Member> getMembers() {
+        return members;
+    }    
+}
